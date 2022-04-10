110510651d90ba6e822234b89eecc1b28028067f
tawatchai7034 <tawatchaisunanta@gmail.com>
2022-04-10 21:48:44 +0700

    'successfully'

---
+++ node-backend/index.js
@@ -0,0 +1,58 @@
+let express = require("express"),
+  path = require("path"),
+  mongoose = require("mongoose"),
+  cors = require("cors"),
+  bodyParser = require("body-parser"),
+  mongoDb = require("./database/db");
+
+mongoose.Promise = global.Promise;
+
+mongoose
+  .connect(mongoDb.db, {
+    useNewUrlParser: true,
+    // error : MongoParseError: option usefindandmodify is not supported
+    // useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(
+    () => {
+      console.log("Database seccessfully connected");
+    },
+    (error) => {
+      console.log("Database error: " + error);
+    }
+  );
+
+const bookRoute = require("./routes/book.routes");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+// static directory path
+
+app.use(express.static(path.join(__dirname,'dist/')))
+
+// Base route
+
+app.get('/',(req, res) => {
+    res.sendFile(path.join(__dirname,'dist/index.html'))
+})
+
+// API route
+app.use('/api',bookRoute);
+
+// port
+const port = process.env.PORT || 8000;
+
+app.listen(port, ()=>{
+    console.log('listening on port '+port);
+})
+
+// 404 Handler
+app.use(function(err,req, res, next) {
+    console.err(ree.message);
+    if(!err.statusCode) err.statusCode=500;
+    res.status(err.statusCode).send(err.message);
+})
---
+++ src/app/components/book-list/book-list.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import {CrudService} from './../../service/crud.service';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.css']
+})
+export class BookListComponent implements OnInit {
+  
+  Books:any = [];
+
+  constructor(private crudService: CrudService) { }
+
+  ngOnInit(): void {
+    this.crudService.GetBooks().subscribe(res => {
+      console.log(res);
+      this.Books = res;
+    })
+  }
+
+  delete(id: any,i:any){
+    console.log(id);
+    if(window.confirm('Do you want to delete this book?')){
+      this.crudService.deleteBook(id).subscribe(res => {
+        this.Books.splice(i,1)
+      })
+    }
+  }
+
+}
